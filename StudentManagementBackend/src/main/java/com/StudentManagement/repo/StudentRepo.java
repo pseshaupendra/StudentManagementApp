adeaffb50cbec90d5587fc92fbbffac5aef029d3
SESHA UPENDRA <pseshaupendra@gmail.com>
2024-08-26 20:30:41 +0530

    First commit

---
+++ StudentManagementBackend/src/main/java/com/StudentManagement/repo/StudentRepo.java
@@ -0,0 +1,10 @@
+package com.StudentManagement.repo;
+
+import com.StudentManagement.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+}
---
+++ StudentManagementBackend/src/main/java/com/StudentManagement/service/StudentServiceImpl.java
@@ -0,0 +1,45 @@
+package com.StudentManagement.service;
+
+import com.StudentManagement.Student;
+import com.StudentManagement.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements IStudentService
+{
+
+    @Autowired
+    private StudentRepo repo;
+
+    @Override
+    public Student saveStudent(Student student) {
+        //Invock Logic to store data in database
+
+        repo.save(student);
+
+        return student;
+    }
+
+    @Override
+    public List<Student> getAllStudents() {
+        //Invock Logic to retrive all student records from database
+        return  repo.findAll();
+    }
+
+    @Override
+    public Student getStudentById(Integer id) {
+
+        Optional<Student> optional=repo.findById(id);
+       return optional.get();
+
+    }
+
+    @Override
+    public void deleteStudentById(Integer id) {
+        repo.deleteById(id);
+    }
+}
